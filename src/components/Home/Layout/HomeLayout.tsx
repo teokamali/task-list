7f349cb0cffbfd844286c5fdfa5b31dbe6407079
Mammad Kamalipour <mohammad.kp8093@gmail.com>
2023-08-17 19:43:33 +0330

    header added again

--- src/components/Home/Layout/HomeLayout.tsx
+++ src/components/Home/Layout/HomeLayout.tsx
@@ -1,4 +1,5 @@
 import { useBaseComponent } from '@base/BaseComponent';
+import { HomeHeader } from '../Header/HomeHeader';
 import { StyledHomeBodyWrapper, StyledHomeLayout } from './HomeLayoutStyle';
 import { IHomeLayoutProps } from './HomeLayoutType';
 
@@ -6,7 +7,7 @@ export const HomeLayout = (props: IHomeLayoutProps) => {
   useBaseComponent();
   const { children } = props;
   return <StyledHomeLayout>
-    {/* <HomeHeader /> */}
+    <HomeHeader />
     <StyledHomeBodyWrapper>
       {children}
     </StyledHomeBodyWrapper>
